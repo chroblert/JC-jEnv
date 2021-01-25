a95847aecb7e140634bb380e6b62ee342b0c8a0c
Jerrybird <jerryzvs@163.com>
2021-01-25 17:23:36 +0800

    使用echo|set /p替代直接使用echo输出

--- jEnv.bat
+++ jEnv.bat
@@ -7,21 +7,25 @@ chcp 65001
 @REM set JC_jEnv=JC_jEnv
 
 if "%1" == "local" (
-    echo [+] jenv local alias
+    echo |set /p JCOut="[+] jenv local alias"
+    echo.
     goto switch_local 
 )
 
 if "%1" == "global" (
-    echo [+] jenv global alias
+    echo | set /p JCOut=" [+] jenv global alias"
+    echo.
     goto switch_global 
 )
 
 if "%1" == "add" (
-    echo [+] jenv add jdk_dir alias
+    echo | set /p JCOut=" [+] jenv add jdk_dir alias"
+    echo.
     goto addjdk
 )
 if "%1" == "del" (
-    echo [+] jenv del alias
+    echo | set /p JCOut=" [+] jenv del alias"
+    echo.
     goto deljdk
 )
 
@@ -30,45 +34,69 @@ if "%1" == "version" (
 )
 
 :usage
+    @REM echo | set /p JCOut=" ================================================"
     echo ================================================
-    echo ^| JC-jEnv
-    echo ^| Despt:  windows Java环境管理工具
-    echo ^| Author: JC0o0l,Jerrybird
-    echo ^| Repo:   https://github.com/chroblert/JC-jEnv.git
+    @REM echo.
+    echo | set /p JCOut=" | JC-jEnv"
+    echo.
+    echo | set /p JCOut=" | Despt:  windows Java环境管理工具"
+    echo.
+    echo | set /p JCOut=" | Author: JC0o0l,Jerrybird"
+    echo.
+    echo | set /p JCOut=" | Repo:   https://github.com/chroblert/JC-jEnv.git"
+    echo.
+    @REM echo | set /p JCOut=" ================================================"
     echo ================================================
     echo.
-    echo 使用说明
-    echo  jenv [options]
-    echo       version 
-    echo         - 显示当前所有的java版本
-    echo       local alias 
-    echo         - 设置java版本，只在当前shell下起作用
-    echo       global alias 
-    echo         - 设置java版本，在全局下都起作用
-    echo       add 目录 alias 
-    echo         - add a version 
-    echo       del alias     
-    echo         - delete a version
+    echo | set /p JCOut=" 使用说明"
+    echo.
+    echo | set /p JCOut="  jenv [options]"
+    echo.
+    echo | set /p JCOut="       version "
+    echo.
+    echo | set /p JCOut="         - 显示当前所有的java版本"
+    echo.
+    echo | set /p JCOut="       local alias "
+    echo.
+    echo | set /p JCOut="         - 设置java版本，只在当前shell下起作用"
+    echo.
+    echo | set /p JCOut="       global alias "
+    echo.
+    echo | set /p JCOut="         - 设置java版本，在全局下都起作用"
+    echo.
+    echo | set /p JCOut="       add 目录 alias "
+    echo.
+    echo | set /p JCOut="         - add a version "
+    echo.
+    echo | set /p JCOut="       del alias     "
+    echo.
+    echo | set /p JCOut="         - delete a version"
+    echo.
     goto end
 
 :version
     if "%JC_jEnv" == "" (
-        echo 当前没有设置任何版本
+        echo | set /p JCOut=" 当前没有设置任何版本"
+        echo.
         goto usage
     )
     @REM 枚举JC_jEnv环境变量中的值
     echo.
-    echo all version:
+    echo | set /p JCOut=" all version:"
+    echo.
     set remain=%JC_jEnv%
 :loop
     for /f "delims=;, tokens=1*" %%i in ("%remain%") do (
-        @REM echo %%i
+        @REM echo | set /p JCOut=" %%i"
+        @REM echo.
         call set value=%%%%i%%
         set var=%%i
         if /i "%value%" equ "%JAVA_HOME%" (
-            echo * %var% %value%
+            echo | set /p JCOut=" * %var% %value%"
+            echo.
         ) else (
-            echo   %var% %value%
+            echo | set /p JCOut="   %var% %value%"
+            echo.
         )
         set remain=%%j
         @REM goto loop
@@ -97,7 +125,8 @@ if "%1" == "version" (
     )
     set "path=%java_path%;%path%"
     set "JAVA_HOME=%TMP_JAVAHOME%"
-    echo  已切换到%2 %java_path%
+    echo | set /p JCOut="  已切换到%2 %java_path%"
+    echo.
     @REM rem 设置别名
     %SystemRoot%\system32\doskey %2=%JAVA_HOME%\bin\java.exe $*   >null
     set java_path=
@@ -119,24 +148,30 @@ if "%1" == "version" (
     )
     REM 设置java_home环境变量
     reg delete HKCU\Environment /v JAVA_HOME /f >null
-    echo  .使用reg删除以前的JAVA_HOME %JAVA_HOME%
+    echo | set /p JCOut="  .使用reg删除以前的JAVA_HOME %JAVA_HOME%"
+    echo.
     reg add HKCU\Environment  /v JAVA_HOME /t REG_SZ /d %TMP_JAVAHOME%  /f >null
-    echo  .使用reg创建新的JAVA_HOME %TMP_JAVAHOME%
+    echo | set /p JCOut="  .使用reg创建新的JAVA_HOME %TMP_JAVAHOME%"
+    echo.
     setx JAVA_HOME %TMP_JAVAHOME% >null
-    echo  .使用setx更新JAVA_HOME
+    echo | set /p JCOut="  .使用setx更新JAVA_HOME"
+    echo.
     @REM 更新本shell中path
     set "path=%java_path%;%path%"
     set "JAVA_HOME=%TMP_JAVAHOME%"
     @REM rem 设置别名
     %SystemRoot%\system32\doskey %2=%JAVA_HOME%\bin\java.exe $*   >null
-    @REM echo "%path%"
+    @REM echo | set /p JCOut=" "%path%""
+    @REM echo.
     set java_path=
     set TMP_JAVAHOME=
     goto end
 
 :setpathusereg
-    echo 使用注册表设置PATH环境变量
-    echo %~1   %~2
+    echo | set /p JCOut=" 使用注册表设置PATH环境变量"
+    echo.
+    echo | set /p JCOut=" %~1   %~2"
+    echo.
     reg delete HKCU\Environment /v %~1 /f
     reg add HKCU\Environment  /v %~1 /t REG_SZ /d "%~2"  /f
     call refreshenv
@@ -154,7 +189,8 @@ if "%1" == "version" (
     set %~2=
     goto refreshenv
 :addjdk
-    rem echo [+] jenv add jdk_dir alias
+    rem echo | set /p JCOut=" [+] jenv add jdk_dir alias"
+    rem echo.
     if "%2" == "" goto end
     if "%2" == "" goto end
     set JDK_ALIAS=%3
@@ -162,33 +198,42 @@ if "%1" == "version" (
     @REM 判断是否存在JDK_ALIAS别名
     call set t_temp=%%%JDK_ALIAS%%%
     if NOT "%t_temp%" == "" (
-        echo %JDK_ALIAS%已经存在
+        echo | set /p JCOut=" %JDK_ALIAS%已经存在"
+        echo.
         goto end
     )
     rem 添加至用户环境变量中
     %SystemRoot%\system32\setx %JDK_ALIAS% %JDK_DIR% >null
-    @REM echo/%SystemRoot%\system32\setx %JDK_ALIAS% %JDK_DIR% >>f:/JC_jEnv.cmd
-    echo "%JC_jEnv%"|%SystemRoot%\system32\findstr "%JDK_ALIAS%" >nul
+    @REM echo | set /p JCOut="/%SystemRoot%\system32\setx %JDK_ALIAS% %JDK_DIR% >>f:/JC_jEnv.cmd"
+    @REM echo.
+    echo | set /p JCOut=" "%JC_jEnv%"|%SystemRoot%\system32\findstr "%JDK_ALIAS%" >nul"
+    echo.
     set notexist=%errorlevel%
     @REM 环境变量JC_jEnv中已存在当前要添加JAVA_ALIAS
     if %notexist% == 0 (
-        echo 已设置过该版本的java
+        echo | set /p JCOut=" 已设置过该版本的java"
+        echo.
         goto end
     )
     @REM 保存添加的环境变量到JC_jEnv中
     if "%JC_jEnv%" == "" (
-        echo 用户环境变量中没有JC_jEnv
+        echo | set /p JCOut=" 用户环境变量中没有JC_jEnv"
+        echo.
         %SystemRoot%\system32\setx JC_jEnv "%JDK_ALIAS%" >null
-        @REM echo/%SystemRoot%\system32\setx JC_jEnv "%JDK_ALIAS%" >>f:/JC_jEnv.cmd
+        @REM echo | set /p JCOut="/%SystemRoot%\system32\setx JC_jEnv "%JDK_ALIAS%" >>f:/JC_jEnv.cmd"
+        @REM echo.
     ) else (
         %SystemRoot%\system32\setx JC_jEnv "%JDK_ALIAS%;%JC_jEnv%" >null
-        @REM echo/%SystemRoot%\system32\setx JC_jEnv "%JDK_ALIAS%;%JC_jEnv%" >>f:/JC_jEnv.cmd
+        @REM echo | set /p JCOut="/%SystemRoot%\system32\setx JC_jEnv "%JDK_ALIAS%;%JC_jEnv%" >>f:/JC_jEnv.cmd"
+        @REM echo.
     )
     @REM rem 设置别名
     %SystemRoot%\system32\doskey %JDK_ALIAS%=%JDK_DIR%\bin\java.exe $*   >null
-    @REM echo/%SystemRoot%\system32\doskey %JDK_ALIAS%=%JDK_DIR%\bin\java.exe $* >>f:/JC_jEnv.cmd
+    @REM echo | set /p JCOut="/%SystemRoot%\system32\doskey %JDK_ALIAS%=%JDK_DIR%\bin\java.exe $* >>f:/JC_jEnv.cmd"
+    @REM echo.
 
-    echo 已为%JDK_DIR%\bin\java.exe设置别名%JDK_ALIAS%
+    echo | set /p JCOut=" 已为%JDK_DIR%\bin\java.exe设置别名%JDK_ALIAS%"
+    echo.
     @REM 清空定义的变量
     @REM set JDK_ALIAS=
     set JDK_DIR=
